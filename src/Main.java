a3ca3ef0b72e2441f22a90e948a3d103c2dafcc6
PetrovaEkat <katisyn853@gmail.com>
2023-02-28 18:48:59 +0400

    Выполнено ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,61 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void checkYearForLeap(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        checkYearForLeap(2005);
+    }
+
+    public static void checkOsAndYerOfPhone (int clientDeviceYear, int clientOS) {
+
+        if (clientOS == 0 && clientDeviceYear > 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+
+        } else if (clientOS == 0 && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+
+        } else if (clientOS == 1 && clientDeviceYear > 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+
+        } else if (clientOS == 1 && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+        checkOsAndYerOfPhone(2009, 0);
+    }
+
+    public static void checkDeliveryRules (int deliveryDistance) {
+
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: 1");
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            System.out.println("Потребуется дней: 2");
+        } else if (deliveryDistance > 60 && deliveryDistance <=100) {
+            System.out.println("Потребуется дней: 3");
+        } else {
+            System.out.println("Доставки нет");
+        }
+    }
+
+    public static void task3 () {
+        System.out.println("Задача 3");
+        checkDeliveryRules(95);
     }
 }
